93d2bce2681d4837ad566f65d081ce99a63298fc
Vishal Nema <vishalnema23@gmail.com>
2025-10-10 11:09:51 +0530

    MultiThreading in JAVA Day1(Methods --- join)

--- src/MultiThreading/MyThread.java
+++ src/MultiThreading/MyThread.java
@@ -14,8 +14,9 @@ public class MyThread extends Thread {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         MyThread t1 = new MyThread();
         t1.start();
+        t1.join();
     }
 }
