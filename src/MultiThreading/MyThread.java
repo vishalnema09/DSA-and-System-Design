b2d635ac4087ee90df9c13504dd03649c9d9649b
Vishal Nema <vishalnema23@gmail.com>
2025-10-10 11:08:41 +0530

    MultiThreading in JAVA Day1(Methods --- start)

--- src/MultiThreading/MyThread.java
+++ src/MultiThreading/MyThread.java
@@ -1,24 +1,13 @@
 package MultiThreading;
 
 public class MyThread extends Thread {
-
     @Override
     public void run() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        System.out.println("Thread is running");
     }
 
-    public static void main(String[] args)  throws InterruptedException{
+    public static void main(String[] args) {
         MyThread t1 = new MyThread();
-        System.out.println(t1.getState());
         t1.start();
-        System.out.println(t1.getState());
-        Thread.sleep(100);
-        System.out.println(t1.getState());
-        t1.join();
-        System.out.println(t1.getState());
     }
 }
