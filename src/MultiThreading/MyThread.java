f3589194e7a985bdd52553e0e2bdbc1fd687cdff
Vishal Nema <vishalnema23@gmail.com>
2025-10-10 11:44:52 +0530

    MultiThreading in JAVA Day1(Methods --- Interrupted )

--- src/MultiThreading/MyThread.java
+++ src/MultiThreading/MyThread.java
@@ -2,33 +2,19 @@ package MultiThreading;
 
 public class MyThread extends Thread {
 
-    public MyThread(String name){
-        super(name);
-    }
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(Thread.currentThread().getName() + " - priority: " + Thread.currentThread().getPriority() + " - count: "+ i);
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        try {
+            Thread.sleep(1000);
+            System.out.println("Thread is running");
+        } catch (InterruptedException e) {
+            System.out.println("Thread Interrupted: "+ e);
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-
-        //NO STRICT RULE JUST HINT
-
-        MyThread l = new MyThread("low");
-        MyThread m = new MyThread("medium");
-        MyThread h = new MyThread("high");
-        l.setPriority(Thread.MIN_PRIORITY);
-        m.setPriority(Thread.NORM_PRIORITY);
-        h.setPriority(Thread.MAX_PRIORITY);
-        l.start();
-        m.start();
-        h.start();
+        MyThread t1 = new MyThread();
+        t1.start();
+        t1.interrupt();
     }
 }
