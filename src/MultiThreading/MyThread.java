1290b0d8a8298452a7a2f65a930b66c690609c72
Vishal Nema <vishalnema23@gmail.com>
2025-10-10 11:37:42 +0530

    MultiThreading in JAVA Day1(Methods --- Set Priority and change the thread name)

--- src/MultiThreading/MyThread.java
+++ src/MultiThreading/MyThread.java
@@ -1,22 +1,34 @@
 package MultiThreading;
 
 public class MyThread extends Thread {
+
+    public MyThread(String name){
+        super(name);
+    }
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - priority: " + Thread.currentThread().getPriority() + " - count: "+ i);
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println(i);
-
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        MyThread t1 = new MyThread();
-        t1.start();
-        t1.join();
+
+        //NO STRICT RULE JUST HINT
+
+        MyThread l = new MyThread("low");
+        MyThread m = new MyThread("medium");
+        MyThread h = new MyThread("high");
+        l.setPriority(Thread.MIN_PRIORITY);
+        m.setPriority(Thread.NORM_PRIORITY);
+        h.setPriority(Thread.MAX_PRIORITY);
+        l.start();
+        m.start();
+        h.start();
     }
 }
