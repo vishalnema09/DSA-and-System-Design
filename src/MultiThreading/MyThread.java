1889a534db46ba32a70a55964d1a6934825385da
Vishal Nema <vishalnema23@gmail.com>
2025-10-10 11:09:15 +0530

    MultiThreading in JAVA Day1(Methods --- sleep)

--- src/MultiThreading/MyThread.java
+++ src/MultiThreading/MyThread.java
@@ -3,7 +3,15 @@ package MultiThreading;
 public class MyThread extends Thread {
     @Override
     public void run() {
-        System.out.println("Thread is running");
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i);
+
+        }
     }
 
     public static void main(String[] args) {
